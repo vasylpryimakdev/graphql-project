f68d2a2e4c60b8cca335416039536520f8f1b714
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-10 18:49:50 +0300

    feat: add cursor-based pagination to queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -4,7 +4,8 @@ const Query = {
     users(parent, args, { prisma }, info) {
         const opArgs = {
             first: args.first,
-            skip: args.skip
+            skip: args.skip,
+            after: args.after
         }
         
         if (args.query) {
@@ -20,6 +21,9 @@ const Query = {
     myPosts(parent, args, { prisma, request }, info) {
         const userId = getUserId(request)
         const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
             where: {
                 author: {
                     id: userId
@@ -41,6 +45,7 @@ const Query = {
         const opArgs = {
             first: args.first,
             skip: args.skip,
+            after: args.after,
             where: {
                 published: true
             }
@@ -57,7 +62,13 @@ const Query = {
         return prisma.query.posts(opArgs, info)
     },
     comments(parent, args, { prisma }, info) {
-        return prisma.query.comments(null, info)
+        const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after
+        }
+
+        return prisma.query.comments(opArgs, info)
     },
     me(parent, args, { prisma, request }, info) {
         const userId = getUserId(request)
