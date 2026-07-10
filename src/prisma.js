e817f6dc50042fb7826066130a52e91d31548bb1
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-10 17:54:16 +0300

    refactor: extract Prisma mutation helper functions

--- src/prisma.js
+++ src/prisma.js
@@ -7,49 +7,48 @@ const prisma = new Prisma({
 
 // prisma.query prisma.mutation prisma.subscription prisma.exists
 
-// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
+const createPostForUser = async (authorId, data) => {
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
+            }
+        }
+    }, '{ id }')
+    const user = await prisma.query.user({
+        where: {
+            id: authorId
+        }
+    }, '{ id name email posts { id title published } }')
+    return user
+}
+
+// createPostForUser('cjjucl3yu004x0822dq5tipuz', {
+//     title: 'Great books to read',
+//     body: 'The War of Art',
+//     published: true
+// }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
 // })
 
-// prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
+const updatePostForUser = async (postId, data) => {
+    const post = await prisma.mutation.updatePost({
+        where: {
+            id: postId
+        },
+        data
+    }, '{ author { id } }')
+    const user = await prisma.query.user({
+        where: {
+            id: post.author.id
+        }
+    }, '{ id name email posts { id title published } }')
+    return user
+}
+
+// updatePostForUser("cjjzwjkez009p0822fbvn6lui", { published: false }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
 // })
-
-// prisma.mutation.createPost({
-//     data: {
-//         title: "GraphQL 101",
-//         body: "",
-//         published: false,
-//         author: {
-//             connect: {
-//                 id: "cjjybkwx5006h0822n32vw7dj"
-//             }
-//         }
-//     }
-// }, '{ id title body published }').then((data) => {
-//     console.log(data)
-//     return prisma.query.users(null, '{ id name posts { id title } }')
-// }).then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
-
-//
-// Goal: Mess around with mutations
-//
-// 1. Update the newly created post changing it's body and marking it as published
-// 2. Fetch all posts (id, title, body, published) and print them to the console
-// 3. View the list of posts and confirm that post did have it body and published values updated
-
-prisma.mutation.updatePost({
-    where: {
-        id: "cjjzr3qpi00840822b1qpv8a8"
-    },
-    data: {
-        body: "This is how to get started with Graphql...",
-        published: true
-    }
-}, '{ id }').then((data) => {
-    return prisma.query.posts(null, '{ id title body published }')
-}).then((data) => {
-    console.log(data)
-})
