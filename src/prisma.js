90144f27fbb923a86ee0487493fa24ebbe3d2ef3
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-10 17:53:48 +0300

    feat: add Prisma mutation examples

--- src/prisma.js
+++ src/prisma.js
@@ -7,10 +7,49 @@ const prisma = new Prisma({
 
 // prisma.query prisma.mutation prisma.subscription prisma.exists
 
-prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
-    console.log(JSON.stringify(data, undefined, 2))
-})
+// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
 
-prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
-    console.log(JSON.stringify(data, undefined, 2))
+// prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+
+// prisma.mutation.createPost({
+//     data: {
+//         title: "GraphQL 101",
+//         body: "",
+//         published: false,
+//         author: {
+//             connect: {
+//                 id: "cjjybkwx5006h0822n32vw7dj"
+//             }
+//         }
+//     }
+// }, '{ id title body published }').then((data) => {
+//     console.log(data)
+//     return prisma.query.users(null, '{ id name posts { id title } }')
+// }).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+
+//
+// Goal: Mess around with mutations
+//
+// 1. Update the newly created post changing it's body and marking it as published
+// 2. Fetch all posts (id, title, body, published) and print them to the console
+// 3. View the list of posts and confirm that post did have it body and published values updated
+
+prisma.mutation.updatePost({
+    where: {
+        id: "cjjzr3qpi00840822b1qpv8a8"
+    },
+    data: {
+        body: "This is how to get started with Graphql...",
+        published: true
+    }
+}, '{ id }').then((data) => {
+    return prisma.query.posts(null, '{ id title body published }')
+}).then((data) => {
+    console.log(data)
 })
