5d10f54f13b07a06222434843d412d8bfd4662bb
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-13 16:31:51 +0300

    test: add integration tests for public queries

--- tests/user.test.js
+++ tests/user.test.js
@@ -68,3 +68,37 @@ test('Should create a new user', async () => {
     const exists = await prisma.exists.User({ id: response.data.createUser.user.id })
     expect(exists).toBe(true)
 })
+
+test('Should expose public author profiles', async () => {
+    const getUsers = gql`
+        query {
+            users {
+                id
+                name
+                email
+            }
+        }
+    `
+    const response = await client.query({ query: getUsers })
+
+    expect(response.data.users.length).toBe(1)
+    expect(response.data.users[0].email).toBe(null)
+    expect(response.data.users[0].name).toBe('Jen')
+})
+
+test('Should expose published posts', async () => {
+    const getPosts = gql`
+        query {
+            posts {
+                id
+                title
+                body
+                published
+            }
+        }
+    `
+    const response = await client.query({ query: getPosts })
+
+    expect(response.data.posts.length).toBe(1)
+    expect(response.data.posts[0].published).toBe(true)
+})
