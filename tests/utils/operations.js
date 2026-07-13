05a447a520676387f233df194a7758f1229da4b0
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-13 16:38:49 +0300

    test: extract reusable GraphQL operations for tests

--- tests/utils/operations.js
+++ tests/utils/operations.js
@@ -45,4 +45,63 @@ const getProfile = gql`
     }
 `
 
-export { createUser, login, getUsers, getProfile }
+const getPosts = gql`
+    query {
+        posts {
+            id
+            title
+            body
+            published
+        }
+    }
+`
+
+const myPosts = gql`
+    query {
+        myPosts {
+            id
+            title
+            body
+            published
+        }
+    }
+`
+
+const updatePost = gql`
+    mutation($id: ID!, $data: UpdatePostInput!) {
+        updatePost(
+            id: $id,
+            data: $data
+        ){
+            id
+            title
+            body
+            published
+        }
+    }
+`
+
+const createPost = gql`
+    mutation($data: CreatePostInput!) {
+        createPost(
+            data: $data
+        ){
+            id
+            title
+            body
+            published
+        }
+    }
+`
+
+const deletePost = gql`
+        mutation($id: ID!) {
+            deletePost(
+                id: $id
+            ) {
+                id
+            }
+        }
+    `
+
+export { createUser, login, getUsers, getProfile, getPosts, myPosts, updatePost, createPost, deletePost }
