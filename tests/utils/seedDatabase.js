8ceb41f64cf0e27b4b34295c8030eafcc1fcefff
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-13 16:37:22 +0300

    test: add integration tests for post mutations

--- tests/utils/seedDatabase.js
+++ tests/utils/seedDatabase.js
@@ -12,6 +12,24 @@ const userOne = {
     jwt: undefined
 }
 
+const postOne = {
+    input: {
+        title: 'My published post',
+        body: '',
+        published: true
+    },
+    post: undefined
+}
+
+const postTwo = {
+    input: {
+        title: 'My draft post',
+        body: '',
+        published: false
+    },
+    post: undefined
+}
+
 const seedDatabase = async () => {
     // Delete test data
     await prisma.mutation.deleteManyPosts()
@@ -23,11 +41,10 @@ const seedDatabase = async () => {
     })
     userOne.jwt = jwt.sign({ userId: userOne.user.id }, process.env.JWT_SECRET)
 
-    await prisma.mutation.createPost({
+    // Create post one
+    postOne.post = await prisma.mutation.createPost({
         data: {
-            title: 'My published post',
-            body: '',
-            published: true,
+            ...postOne.input,
             author: {
                 connect: {
                     id: userOne.user.id
@@ -35,11 +52,11 @@ const seedDatabase = async () => {
             }
         }
     })
-    await prisma.mutation.createPost({
+
+    // Create post two
+    postTwo.post = await prisma.mutation.createPost({
         data: {
-            title: 'My draft post',
-            body: '',
-            published: false,
+            ...postTwo.input,
             author: {
                 connect: {
                     id: userOne.user.id
@@ -49,4 +66,4 @@ const seedDatabase = async () => {
     })
 }
 
-export { seedDatabase as default, userOne }
+export { seedDatabase as default, userOne, postOne, postTwo }
