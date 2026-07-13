50ffe3c4c9a2aa25aa2fb4691faf395b72d9d583
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-13 16:31:21 +0300

    test: seed database before integration tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,9 +1,46 @@
 import 'cross-fetch/polyfill'
 import ApolloBoost, { gql } from 'apollo-boost'
+import bcrypt from 'bcryptjs'
 import prisma from '../src/prisma'
 
 const client = new ApolloBoost({
     uri: 'http://localhost:4000'
+})
+
+beforeEach(async () => {
+    await prisma.mutation.deleteManyPosts()
+    await prisma.mutation.deleteManyUsers()
+    const user = await prisma.mutation.createUser({
+        data: {
+            name: 'Jen',
+            email: 'jen@example.com',
+            password: bcrypt.hashSync('Red098!@#$')
+        }
+    })
+    await prisma.mutation.createPost({
+        data: {
+            title: 'My published post',
+            body: '',
+            published: true,
+            author: {
+                connect: {
+                    id: user.id
+                }
+            }
+        }
+    })
+    await prisma.mutation.createPost({
+        data: {
+            title: 'My draft post',
+            body: '',
+            published: false,
+            author: {
+                connect: {
+                    id: user.id
+                }
+            }
+        }
+    })
 })
 
 test('Should create a new user', async () => {
